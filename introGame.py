4db5adf315f8ee6711f2c1a6ee544e2b30252ab9
Ignacio Ulloa <i.ulloarod@gmail.com>
2024-03-15 11:15:03 -0300

    creates an screen surface for the game to run in to

--- introGame.py
+++ introGame.py
@@ -1,4 +1,13 @@
+#adding the pygame lib to use as a game engine.
 import pygame
 
+#initializing pygame lib
 pygame.init()
+#creating a display surface for pygame with 1 fps rendered and an 800x400 screen size(width and height). 
 screen = pygame.display.set_mode((800,400))
+#to continue playing the game (continuous fps rendered), all of my code needs to be inside a while loop. 
+while True: #keeps the game running indefinetly. Draw all elements and update everything
+    for event in pygame.event.get(): #catches all events
+        if event.type == pygame.quit:
+            pygame.quit() #for the user to quit the game
+    pygame.display.update() #updates the display surface
