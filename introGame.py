42eda13bcd3e5fe22c135aea30f8dbc9808f3657
Ignacio Ulloa <i.ulloarod@gmail.com>
2024-03-15 11:34:16 -0300

    adds a way to close the game's window safely

--- introGame.py
+++ introGame.py
@@ -1,13 +1,12 @@
-#adding the pygame lib to use as a game engine.
-import pygame
+import pygame #adding the pygame lib to use as a game engine.
+from sys import exit #to safely interrupt code from running (helps quiting the game)
 
-#initializing pygame lib
-pygame.init()
-#creating a display surface for pygame with 1 fps rendered and an 800x400 screen size(width and height). 
-screen = pygame.display.set_mode((800,400))
-#to continue playing the game (continuous fps rendered), all of my code needs to be inside a while loop. 
+pygame.init() #initializing pygame lib
+screen = pygame.display.set_mode((800,400)) #creating a display surface for pygame with 1 fps rendered and an 800x400 screen size(width and height). 
+
 while True: #keeps the game running indefinetly. Draw all elements and update everything
-    for event in pygame.event.get(): #catches all events
-        if event.type == pygame.quit:
-            pygame.quit() #for the user to quit the game
+    for event in pygame.event.get(): #event catcher
+        if event.type == pygame.QUIT:
+            pygame.quit() #for the user to quit the game. This creates an error because python can't keep up, but it's solved with the "exit" function
+            exit() #ends the game. at least on windows
     pygame.display.update() #updates the display surface
