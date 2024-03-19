5b17f2c21e598933b9b1e66a8b6ba3bdc57d4dc1
Ignacio Ulloa <i.ulloarod@gmail.com>
2024-03-19 20:59:28 -0300

    added rectangles for surfaces and colliders

--- introGame.py
+++ introGame.py
@@ -8,12 +8,15 @@ clock = pygame.time.Clock() #creating a Clock obj to cap the game's fps
 test_font = pygame.font.Font('font/Pixeltype.ttf', 50) #params: font,size
 
 #surfaces
-sky_surface = pygame.image.load('graphics/Sky.png')
-ground_surface = pygame.image.load('graphics/ground.png')
-text_surface = test_font.render('My game', False, 'Green') #params: 'text itself', AntiAliasin, colour
+sky_surface = pygame.image.load('graphics/Sky.png').convert()
+ground_surface = pygame.image.load('graphics/ground.png').convert()
+text_surface = test_font.render('My game', False, 'Green') #params: 'text itself', AntiAliasing, colour
 #adding a movable surface
-snail_surface = pygame.image.load('graphics/snail/snail1.png')
-snail_x_pos = 600
+snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_rect = snail_surf.get_rect(bottomright = (600,300))
+#adding a player surface
+player_surf = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
+player_rect = player_surf.get_rect(midbottom =(80,300)) #create a rectangle using the surface's size
 
 while True: #keeps the game running indefinetly. Draw all elements and update everything
     for event in pygame.event.get(): #event catcher
@@ -24,10 +27,15 @@ while True: #keeps the game running indefinetly. Draw all elements and update ev
     screen.blit(ground_surface,(0,300)) #params: surface object, position
     screen.blit(sky_surface,(0,0))   
     screen.blit(text_surface,(300,50))
-    snail_x_pos -= 4 #to move the snail
-    if snail_x_pos < -100:
-        snail_x_pos = 800
-    screen.blit(snail_surface,(snail_x_pos,275))
+    snail_rect.x -= 4
+    if snail_rect.right <= 0:
+        snail_rect.left = 800
+    screen.blit(snail_surf,(snail_rect))
+    screen.blit(player_surf,player_rect)
+
+    #adding colliders
+    if player_rect.colliderect(snail_rect):
+        print("collision")
 
     pygame.display.update() #updates the display surface
     clock.tick(60) #frames per seccond capped to 60
