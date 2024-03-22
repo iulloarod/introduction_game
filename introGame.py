fdc980e47a8bb4a091e46d110c1f543039767bab
Ignacio Ulloa <i.ulloarod@gmail.com>
2024-03-22 13:42:34 -0300

    added score based on time passed

--- introGame.py
+++ introGame.py
@@ -1,23 +1,32 @@
 import pygame #adding the pygame lib to use as a game engine.
 from sys import exit #to safely interrupt code from running (helps quiting the game)
 
+def display_score():
+    current_time = int (pygame.time.get_ticks() / 1000) - start_time
+    score_surf = test_font.render(f'Score: {current_time}', False, (64,64,64))
+    score_rect = score_surf.get_rect(center = (400,50))
+    screen.blit(score_surf, score_rect)
+
 pygame.init() #initializing pygame lib
 screen = pygame.display.set_mode((800,400)) #creating a display surface for pygame with 1 fps rendered and an 800x400 screen size(width and height). 
 pygame.display.set_caption('introGame') #setting game's name
 clock = pygame.time.Clock() #creating a Clock obj to cap the game's fps
 test_font = pygame.font.Font('font/Pixeltype.ttf', 50) #params: font,size
 game_active = True #adding a game status to get check if the game is running
-
+start_time = 0
 
 #surfaces
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
+
 #creating a score
-score_surf = test_font.render('Score', False, (64,64,64)) #params: 'text itself', AntiAliasing, colour
-score_rect = score_surf.get_rect(center = (400,50))
+# score_surf = test_font.render('Score', False, (64,64,64)) #params: 'text itself', AntiAliasing, colour
+# score_rect = score_surf.get_rect(center = (400,50))
+
 #adding a movable surface
 snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
 snail_rect = snail_surf.get_rect(bottomright = (600,300))
+
 #adding a player surface
 player_surf = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
 player_rect = player_surf.get_rect(midbottom =(80,300)) #create a rectangle using the surface's size
@@ -40,14 +49,16 @@ while True: #keeps the game running indefinetly. Draw all elements and update ev
                 if event.key == pygame.K_SPACE:
                     game_active = True
                     snail_rect.left = 800
+                    start_time = int (pygame.time.get_ticks() / 1000)
 
     if game_active: #the game part
         #screen blit == block image transfer, meaning putting one surface on other 
         screen.blit(ground_surface,(0,300)) #params: surface object, position
         screen.blit(sky_surface,(0,0))   
-        pygame.draw.rect(screen, '#c0e8ec', score_rect) #params: display surface, colour, object, size
-        pygame.draw.rect(screen, '#c0e8ec', score_rect, 10) #to add margins
-        screen.blit(score_surf,score_rect)
+        # pygame.draw.rect(screen, '#c0e8ec', score_rect) #params: display surface, colour, object, size
+        # pygame.draw.rect(screen, '#c0e8ec', score_rect, 10) #to add margins
+        # screen.blit(score_surf,score_rect)
+        display_score()
 
         #snail blit
         snail_rect.x -= 4
