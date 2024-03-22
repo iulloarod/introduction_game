e805ab23af33f7920b40f7ffda0c548d82975eb8
Ignacio Ulloa <i.ulloarod@gmail.com>
2024-03-22 12:34:40 -0300

    added jumping to game

--- introGame.py
+++ introGame.py
@@ -19,49 +19,40 @@ snail_rect = snail_surf.get_rect(bottomright = (600,300))
 #adding a player surface
 player_surf = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
 player_rect = player_surf.get_rect(midbottom =(80,300)) #create a rectangle using the surface's size
+player_grav = 0 #gravity to improve falling feel
 
 while True: #keeps the game running indefinetly. Draw all elements and update everything
     for event in pygame.event.get(): #event catcher
         if event.type == pygame.QUIT: #it's QUIT, not quit.
             pygame.quit() #for the user to quit the game. This creates an error because python can't keep up, but it's solved with the "exit" function
             exit() #ends the game. at least on windows
-        # if event.type == pygame.MOUSEMOTION:
-        #     if player_rect.collidepoint(event.pos):
-        #         print('collision')
+        if event.type == pygame.MOUSEBUTTONDOWN and player_rect.bottom >= 300: #to jump AND jumping only if the player is at ground level
+            if player_rect.collidepoint(event.pos):
+                player_grav = -20 
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:    
-                print('jump')
-        if event.type == pygame.KEYUP:
-            print('key up')
+            if event.key == pygame.K_SPACE and player_rect.bottom >= 300: #to jump AND jumping only if the player is at ground level    
+                player_grav = -20
 
-#blit == block image transfer, meaning putting one surface on other 
+    #screen blit == block image transfer, meaning putting one surface on other 
     screen.blit(ground_surface,(0,300)) #params: surface object, position
     screen.blit(sky_surface,(0,0))   
     pygame.draw.rect(screen, '#c0e8ec', score_rect) #params: display surface, colour, object, size
     pygame.draw.rect(screen, '#c0e8ec', score_rect, 10) #to add margins
-
-
-
     screen.blit(score_surf,score_rect)
-    #moving the snail surface
+
+    #snail blit
     snail_rect.x -= 4
     if snail_rect.right <= 0:
         snail_rect.left = 800
     screen.blit(snail_surf,(snail_rect))
+
+    #player blit
+    player_grav += 1 #to get a constant downforce to our player
+    player_rect.y += player_grav #to get a "real" gravity for falling
+    if player_rect.bottom >= 300: #to add a "collision with ground" without using collisions to save resources.
+        player_rect.bottom = 300 
     screen.blit(player_surf,player_rect)
 
-    #to control player
-    # keys = pygame.key.get_pressed()
-    # if keys[pygame.K_SPACE]:
-    #     print("jump")
-
-    #adding colliders
-    # if player_rect.colliderect(snail_rect):
-    #     print("collision")
-
-    # mouse_pos = pygame.mouse.get_pos()
-    # if player_rect.collidepoint(mouse_pos):
-    #     print(pygame.mouse.get_pressed())
 
     pygame.display.update() #updates the display surface
     clock.tick(60) #frames per seccond capped to 60
